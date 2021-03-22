4403f79e5df6ab56f7020b8dc2a5d2e9175434b6
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-23 02:30:18 +0900

    auth and middleware

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
 const config = require('./config/key');
+const { User } = require('./models/User');
+const { auth } = require('./middleware/auth');
 
 //application/x-www-form-urlencoded 분석 후 가져올 수 있게 함
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,8 +13,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 //application.json 분석 후 가져올 수 있게 함
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-const { User } = require('./models/User');
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -23,7 +22,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('안녕하세e요~ nodemon!'));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   //회원 가입 시 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
 
@@ -37,7 +36,7 @@ app.post('/register', (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email:req.body.email }, (err, user) => {
     if(!user){
@@ -70,5 +69,15 @@ app.post('/login', (req, res) => {
   });
 
 });
+
+
+app.get('/api/users/auth', auth, (req,res) => {
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 뜻
+  res.status(200).json({
+    _id : req.user._id,
+    isAdmin : req.user.role === 0 ? false : true,
+    
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
