bcb1e7f56fd9f69aa25bb70738fb5b23904d7b95
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-23 02:10:38 +0900

    토큰 생성

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -36,8 +37,8 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function(next){
     var user = this;
 
-    //비밀번호를 암호화 시킨다.
     if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
         bcrypt.genSalt(saltRounds, function(err, salt){
             if(err) return next(err);
 
@@ -48,8 +49,32 @@ userSchema.pre('save', function(next){
             });
 
         });
+    }else {
+        next();
     }
 });
+
+userSchema.methods.comprePassword = function(plainPassword, cb) {
+    //plainPassword 1234567     암호화된 비밀번호 $2b$10$Vo1Wu4VKFY7yclgerium9usns.HQHDqRnJqhMYbzZCp4N7e3CDkCW
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken을 이용해 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'secretToken' = token
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user);
+    })
+
+}
 
 const User = mongoose.model('User', userSchema);
 
