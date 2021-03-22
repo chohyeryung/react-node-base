c52a8f3cec15ea68c972b0693d0a224ad3e57879
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-23 01:35:37 +0900

    bcrypt 비밀번호 암호화

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.post('/register', (req, res) => {
   //그것들을 데이터 베이스에 넣어준다.
 
   const user = new User(req.body);  //req.body -> json 형식
+
   user.save((err, userInfo) => {
     if(err) return res.json({success:false, err})
     return res.status(200).json({
