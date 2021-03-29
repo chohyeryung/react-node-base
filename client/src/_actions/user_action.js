4004f4a12b1e68a0b7df016ac694b3a9be9e6b8d
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-29 23:55:27 +0900

    register with redux

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -12,3 +12,13 @@ export function loginUser(dataTosubmit) {
         payload:request
     }
 }
+
+export function registerUser(dataTosubmit) {
+    const request = axios.post('/api/users/register', dataTosubmit)
+        .then(response => response.data)
+
+    return {
+        type:REGISTER_USER,
+        payload:request
+    }
+}
