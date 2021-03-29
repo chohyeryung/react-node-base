49b6b27daf221b52226c9d2001b52d555f29f8cd
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-29 23:42:24 +0900

    login

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,11 @@
+import Axios from 'axios'
+import {useDispatch} from 'react-redux';
 import React, {useState} from 'react'
+import {loginUser} from '../../../_actions/user_action';
 
 function LoginPage() {
+
+    const dispatch = useDispatch()
 
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
@@ -13,16 +18,29 @@ function LoginPage() {
         setPassword(event.currentTarget.value)
     }
 
+    const onSubmitHandler=(event)=>{
+        event.preventDefault(); //page refresh를 막기 위한 용도
+        
+        let body = {
+            email:Email,
+            password:Password
+        }
+
+        dispatch(loginUser(body))
+
+        
+    }
+
     return (
         
         <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height:'100vh' }}>
-            <form style={{ display:'flex', flexDirection:'column' }}>
+            <form style={{ display:'flex', flexDirection:'column' }} onSubmit={onSubmitHandler}>
                 <label>Email</label>
                 <input type="email" value={Email} onChange={onEmailHandler}/>
                 <label>Password</label>
                 <input type="password" value={Password} onChange={onPasswordHandler}/>
                 <br/>
-                <button>Login</button>
+                <button type="submit">Login</button>
             </form>
         </div>
     )
